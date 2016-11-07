f07614001e31a96ce24748ec28b650d99d273d35
Dimitri Gnidash <dimitri.gnidash@gmail.com>
2016-11-06 20:41:31 -0500

    - Only use text area

--- cms/plugins/text/cms_plugins.py
+++ cms/plugins/text/cms_plugins.py
@@ -4,6 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 from models import Text
 from cms.plugins.text.forms import TextForm
 from cms.plugins.text.widgets.wymeditor_widget import WYMEditor
+from django.forms import Textarea
 from cms.plugins.text.utils import plugin_tags_to_user_html
 from django.forms.fields import CharField
 from cms.plugins.text.settings import USE_TINYMCE
@@ -22,11 +23,7 @@ class TextPlugin(CMSPluginBase):
         Returns the Django form Widget to be used for
         the text area
         """
-        if USE_TINYMCE and "tinymce" in settings.INSTALLED_APPS:
-            from cms.plugins.text.widgets.tinymce_widget import TinyMCEEditor
-            return TinyMCEEditor(installed_plugins=plugins)
-        else:
-            return WYMEditor(installed_plugins=plugins)
+        return Textarea()
 
     def get_form_class(self, request, plugins):
         """
